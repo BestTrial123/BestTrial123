c9f7d8e6a59cd1d34871a3fd8e22e122fae0928c
Christopher Reeves <christopherreeves@Christophers-MacBook-Air.local>
2026-08-10 12:47:30 -0500

    fix function to use ES module syntax

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,7 +1,7 @@
-const Stripe = require('stripe');
+import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
   try {
     const { amount, fund } = JSON.parse(event.body);
 
